a9fec8b0116b66d8e0b91a3a9cc1a2250d94bdce
Aristeo Montoya <aristeomontoya98@gmail.com>
2020-05-18 18:02:29 -0600

    Rutas de API
    Inicio de las rutas de la API, realmente no hacen nada util de momento

--- WEB/tapas20/app/Http/Controllers/cadenaController.php
+++ WEB/tapas20/app/Http/Controllers/cadenaController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Cadena;
 
 class cadenaController extends Controller
 {
-    //
+	public function cadena()
+	{
+		return response()->json(Cadena::get(), 200);
+	}
 }
