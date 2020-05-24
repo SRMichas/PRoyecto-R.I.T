66a01f349349e3b79dbe5a5a1b1925879a17c771
Aristeo Montoya <aristeomontoya98@gmail.com>
2020-05-23 21:02:54 -0600

    Retorno de cadenas

--- WEB/tapas20/app/Http/Controllers/cadenaController.php
+++ WEB/tapas20/app/Http/Controllers/cadenaController.php
@@ -14,9 +14,9 @@ class cadenaController extends Controller
 
 	public function store(Request $request)
 	{
-		$semilla = $request->codigo + $request->maquina + time();
+		$semilla = $request->conteo + $request->maquina + time();
 		$codigo = md5($semilla);	//Sí, el código es un hash.
-		return response()->json(['Codigo' => $codigo]);
+		return response()->json(['Codigo' => substr($codigo, 17)]);
 	}
 
 	public function update(Request $request)
