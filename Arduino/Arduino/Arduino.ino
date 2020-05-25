24fdb5649c9b174baed504680093aa939229b22c
Aristeo Montoya <aristeomontoya98@gmail.com>
2020-05-25 00:47:47 -0600

    Refactorización de código

--- Arduino/Arduino/Arduino.ino
+++ Arduino/Arduino/Arduino.ino
@@ -5,94 +5,82 @@ int resultadoSensor = 0;
 int boton = 2;
 int conteo;
 String cadena;
-/*
-   - Ingresar tapas
-   - Conteo de tapas
-   - Envío de conteo a ESP8266
-   - Espera de respuesta del ESP8266
-   - Mostrar respuesta de ESP8266
-*/
+
 void setup() {
-  lcd.begin(16, 2);
-  mostrarMensaje(0);
-  pinMode(2, INPUT);
-  Serial.begin(9600);
+	lcd.begin(16, 2);
+	mostrarMensaje(0);
+	pinMode(2, INPUT);
+	Serial.begin(9600);
 }
 
 void loop() {
-  if (botonPresionado()) {
-    conteoTapas();
-  } else {
-    //Por algún motivo Arduino necesita este else aquí para no volverse loco.
-  }
+	if (botonPresionado()) {
+		conteoTapas();
+	} else {
+		// Por algún motivo Arduino necesita este else aquí para no volverse
+		// loco.
+	}
 }
 
-boolean botonPresionado() {
-  return digitalRead(boton) == LOW;
-}
+boolean botonPresionado() { return digitalRead(boton) == LOW; }
 
 void conteoTapas() {
-  int lecturaSensor;
-  conteo = 0;
-//  Serial.println("Se presionó el botón");
-  delay(100);
-  while (!botonPresionado()) {
-    lecturaSensor = analogRead(A0);
-    if (lecturaSensor > 500) {
-      conteo++;
-    }
-    if (conteo == 0) {
-      mostrarMensaje(1);
-    } else {
-      mostrarMensaje(2);
-    }
-    delay(350);
-  }
-  if (conteo > 0) {
-    comunicarResultado();
-  }
-  mostrarMensaje(0);
+	int lecturaSensor;
+	conteo = 0;
+	delay(100);
+	while (!botonPresionado()) {
+		lecturaSensor = analogRead(A0);
+		if (lecturaSensor > 500) {
+			conteo++;
+		}
+		if (conteo == 0) {
+			mostrarMensaje(1);
+		} else {
+			mostrarMensaje(2);
+		}
+		delay(350);
+	}
+	if (conteo > 0) {
+		comunicarResultado();
+	}
+	mostrarMensaje(0);
 }
 
 void mostrarMensaje(int mensaje) {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  switch (mensaje) {
-    case 0:
-      lcd.print("Presione el");
-      lcd.setCursor(0, 1);
-      lcd.print("boton.");
-      break;
-    case 1:
-      lcd.print("Ingrese las");
-      lcd.setCursor(0, 1);
-      lcd.print("tapas.");
-      break;
-    case 2:
-      lcd.print("Tapas ingresadas");
-      lcd.setCursor(0, 1);
-      lcd.print(conteo);
-      break;
-    case 3:
-      lcd.print("Esperando");
-      lcd.setCursor(0, 1);
-      lcd.print("respuesta");
-      break;
-    case 4:
-      lcd.print("Codigo:");
-      lcd.setCursor(0, 1);
-      lcd.print(cadena);
-      break;
-  }
+	switch (mensaje) {
+		case 0:
+			escribirLCD("Presione el", "boton.");
+			break;
+		case 1:
+			escribirLCD("Ingrese las", "tapas.");
+			break;
+		case 2:
+			escribirLCD("Tapas ingresadas:", "" + conteo);
+			break;
+		case 3:
+			escribirLCD("Esperando", "respuesta");
+			break;
+		case 4:
+			escribirLCD("Codigo:", cadena);
+			break;
+	}
 }
 
-void comunicarResultado() { //Aquí va la comunicación con el ESP8266.
-  mostrarMensaje(3);
-  cadena = "";
-  Serial.print(conteo);
-  while (cadena.equals(""))
-    cadena = Serial.readString();
-//  Serial.println("Cadena recibida: " + cadena);
-  mostrarMensaje(4);
-  delay(10000);
+void comunicarResultado() {  // Aquí va la comunicación con el ESP8266.
+	mostrarMensaje(3);
+	cadena = "";
+	Serial.print(conteo);
+	while (cadena.equals("")) {
+		cadena = Serial.readString();
+	}
+	mostrarMensaje(4);
+	delay(10000);
+}
+
+void escribirLCD(String linea1, String linea2) {
+	lcd.clear();
+	lcd.setCursor(0, 0);
+	lcd.print(linea1);
+	lcd.setCursor(0, 1);
+	lcd.print(linea2);
 }
--- Arduino/esp8266/esp8266.ino
+++ Arduino/esp8266/esp8266.ino
@@ -1,6 +1,6 @@
+#include <ArduinoJson.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
-#include <ArduinoJson.h>
 
 const char* ssid = "UbeeD3C5";
 const char* password = "RandomAccesTaco28";
@@ -9,58 +9,52 @@ String codigo;
 String conteo = "";
 
 void setup() {
-  Serial.begin(9600);
-  WiFi.begin(ssid, password);
+	Serial.begin(9600);
+	WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-  }
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(1000);
+	}
 }
 
-void loop() {
-  esperarArduino();
-}
+void loop() { esperarArduino(); }
 
 void esperarArduino() {
-  conteo = "";
-  while (conteo.equals("")) {
-    if (Serial.available())
-      conteo = Serial.readString();
-  }
-  if (!conteo.equals("")) {
-//    Serial.println("Entró a if. Conteo: " + conteo);
-    conexion();
-    Serial.print(codigo);
-  }
+	conteo = "";
+	while (conteo.equals("")) {
+		if (Serial.available()) {
+			conteo = Serial.readString();
+		}
+	}
+	if (!conteo.equals("")) {
+		conexion();
+		Serial.print(codigo);
+	}
 }
 
 void conexion() {
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-    http.begin("http://9d12bc4d.ngrok.io/api/cadena");
-    http.addHeader("Content-Type", "application/json");
-    String payload = "{\"conteo\": \"" + conteo + "\", \"maquina\": \"" + maquina + "\"}";
-//    Serial.println("Se envía: " + payload);
-    int httpCode = http.POST(payload);
-    if (httpCode > 0) {
-      String recepcion = http.getString();
-//      Serial.println("Se recibe: " + recepcion);
-      char json[recepcion.length() + 1];
-      recepcion.toCharArray(json, recepcion.length());
-//      Serial.print("JSON: ");
-//      Serial.println(json);
-      parserJson(json);
-    } else {
-      codigo = "error...";
-    }
-    http.end();
-  }
+	if (WiFi.status() == WL_CONNECTED) {
+		HTTPClient http;
+		http.begin("http://9d12bc4d.ngrok.io/api/cadena");
+		http.addHeader("Content-Type", "application/json");
+		String payload = "{\"conteo\": \"" + conteo + "\", \"maquina\": \"" +
+		                 maquina + "\"}";
+		int httpCode = http.POST(payload);
+		if (httpCode > 0) {
+			String recepcion = http.getString();
+			char json[recepcion.length() + 1];
+			recepcion.toCharArray(json, recepcion.length());
+			parserJson(json);
+		} else {
+			codigo = "error...";
+		}
+		http.end();
+	}
 }
 
 void parserJson(char* json) {
-  DynamicJsonDocument documento(1024);
-  deserializeJson(documento, json);
-  String auxiliar = documento["Codigo"];
-  codigo = auxiliar;
-  //  Serial.println("Código tras el parser: " + codigo);
+	DynamicJsonDocument documento(1024);
+	deserializeJson(documento, json);
+	String auxiliar = documento["Codigo"];
+	codigo = auxiliar;
 }
